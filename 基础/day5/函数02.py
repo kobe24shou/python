780beed8435155b6caa5571755c4c76903fb84eb
kobe24shou <aishou24@gmail.com>
2018-05-23 16:10:53 +0800

    0523

--- 基础/day5/函数02.py
+++ 基础/day5/函数02.py
@@ -16,3 +16,19 @@ def w(**rgs):
 
 w(**{'name':'alex'}) # 拿到是字典
 
+print("-----------------高阶函数---------------------------------")
+#高阶函数
+#高阶函数是至少满足下列一个条件的函数:
+#接受一个或多个函数作为输入
+#输出一个函数
+
+def add(x, y, f):
+    return f(x) + f(y)
+res = add(3, -6, abs)
+print(res)
+
+def foo():
+    x=3
+    def bar():
+        return x
+    return bar
