7594d47005c4130d6e5effd1af56af06e11d35ce
kobe24shou <aishou24@gmail.com>
2018-05-25 17:19:50 +0800

    0525

--- 基础/day6/装饰器.py
+++ 基础/day6/装饰器.py
@@ -3,3 +3,12 @@
 # Author:ls 
 # aishou24@gmail.com
 # date:2018/5/24
+
+def outer():
+    x = 10
+    def inner(): #条件一 : inner是内部函数
+        print(x)# 条件二 : x 是外部环境的一个变量
+    return  inner() #结论：内部函数inner 就是一个闭包
+
+f=outer()
+
