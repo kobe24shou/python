8ac04d89ee454e11386700f038ba140cd56f205a
kobe24shou <aishou24@gmail.com>
2018-05-28 18:03:11 +0800

    0528

--- 基础/day6/装饰器.py
+++ 基础/day6/装饰器.py
@@ -4,12 +4,15 @@
 # aishou24@gmail.com
 # date:2018/5/24
 
+
 def outer():
     x = 10
-    def inner(): #条件一 : inner是内部函数
-        print(x)# 条件二 : x 是外部环境的一个变量
-    return  inner() #结论：内部函数inner 就是一个闭包
-#inner() 直接这么写，局部变量，全局无法调用
+
+
+    def inner(): # 条件一 : inner是内部函数
+        print(x) # 条件二 : x 是外部环境的一个变量
+    return inner() # 结论：内部函数inner 就是一个闭包
+# inner() 直接这么写，局部变量，全局无法调用
 
 f = outer()
 
