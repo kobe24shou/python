9f0a7098f5d54763a20a3e02cb98dd8cc30645aa
kobe24shou <aishou24@gmail.com>
2018-06-10 17:29:35 +0800

    0609

--- 基础/day8/hashlib_mod.py
+++ 基础/day8/hashlib_mod.py
@@ -4,5 +4,19 @@
 # aishou24@gmail.com
 # date:2018/6/10
 #加密
-import  hashlib
+import hashlib
+import sha
+
+m = hashlib.md5() #
+print(m) #<md5 HASH object @ 0x00000231930DE440>
+
+m.update("helloword".encode('utf8'))  # 把 helloword 使用md5 加密
+print(m.hexdigest())  #16 进制
+
+m.update("llxx".encode('utf8'))  #
+print(m.hexdigest())  #
+
+m2 = hashlib.md5()
+m2.update("hellowordllxx".encode('utf8'))  #
+print(m2.hexdigest())
 
