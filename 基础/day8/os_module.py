209335d4958460dd1659e8efc44b89ebe3834e66
kobe24shou <aishou24@gmail.com>
2018-06-10 16:21:12 +0800

    0609

--- 基础/day8/os_module.py
+++ 基础/day8/os_module.py
@@ -46,7 +46,7 @@ print(sys.version)      #  获取Python解释程序的版本信息
 #sys.maxint         最大的Int值
 print(sys.path) #           返回模块的搜索路径，初始化时使用PYTHONPATH环境变量的值
 print(sys.platform)      # 返回操作系统平台名称
-#sys.stdout.write('please:')
+print(sys.stdout.write('please:'))
 #val = sys.stdin.readline()[:-1]
 
 
